13773960441030adfc84959d16a757e28dd69a9c
victor-duarte <vicsalf@gmail.com>
2022-01-16 19:24:57 -0500

    Create hello-world component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HelloWorldComponent } from "./pages/hello-world/hello-world.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: HelloWorldComponent, path: "hello-world" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
